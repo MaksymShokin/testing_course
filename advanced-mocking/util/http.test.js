9db379c29c84d712d059d888b88a797b9af81951
Maksym Shokin <problaze7@gmail.com>
2022-10-25 22:27:23 +0300

    global-mocking-difficult-part

--- advanced-mocking/util/http.test.js
+++ advanced-mocking/util/http.test.js
@@ -4,6 +4,10 @@ import { sendDataRequest } from './http';
 const responseData = 'responseData';
 const fetchMock = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== 'string') {
+      reject('Not a string');
+    }
+
     const response = {
       ok: true,
       json: () => new Promise((resolve, reject) => resolve(responseData))
@@ -15,7 +19,28 @@ const fetchMock = vi.fn((url, options) => {
 vi.stubGlobal('fetch', fetchMock);
 
 it('should get response from calling a function', async () => {
-  const result = await sendDataRequest({ key: 'test' });
-  console.log(result)
+  let result;
+
+  try {
+    result = await sendDataRequest({ key: 'test' });
+  } catch (error) {
+    result = error;
+  }
+
   expect(result).toEqual(responseData);
+
+  // another way
+  // return expect(sendDataRequest({ key: 'test' })).resolves.toBe(responseData);
+});
+
+it('should convert the data to JSON before sending the request', async () => {
+  let errorMessage;
+
+  try {
+    await sendDataRequest({ key: 'test' });
+  } catch (error) {
+    errorMessage = error;
+  }
+
+  return expect(errorMessage).not.toBe('Not a string');
 });
