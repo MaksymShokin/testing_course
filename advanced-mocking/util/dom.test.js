05d5d4ee9f647fa1d2553ddf5cd32f43dd667dcc
Maksym Shokin <problaze7@gmail.com>
2022-10-30 15:22:53 +0200

    final-touches

--- advanced-mocking/util/dom.test.js
+++ advanced-mocking/util/dom.test.js
@@ -1,4 +1,4 @@
-import { expect, it, vi } from 'vitest';
+import { beforeEach, expect, it, vi } from 'vitest';
 import fs from 'fs';
 import path from 'path';
 
@@ -10,12 +10,33 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal('document', document);
 
-it('should to have message text content', () => {
+// reset document
+beforeEach(() => {
+  document.body.innerHTML = '';
+  document.write(htmlDocumentContent);
+});
+
+it('should not be null after setting error', () => {
+  const errorText = 'super-error-text';
+  showError(errorText);
+
+  expect(document.getElementById('errors').innerHTML).not.toBeNull();
+});
+
+it('should have the correct message text content', () => {
   const errorText = 'super-error-text';
   showError(errorText);
 
   expect(document.getElementById('errors').innerHTML).toContain(errorText);
+});
+
+it('should have correct message text content in the first child of error paragraphs', () => {
+  const errorText = 'super-error-text';
+  showError(errorText);
+
+  expect(
+    document.getElementById('errors').firstElementChild.textContent
+  ).toEqual(errorText);
 });
